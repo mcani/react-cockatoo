8d375b1fd194e536adc4439704618d73551d01af
Mikhail Anikin <zelebmike@gmail.com>
2023-01-20 18:05:10 -0800

    First lesson

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
 
+const todoList = [
+    {
+    title: 'To finish assignment',
+    objectID: 0,
+    },
+    {
+    title: 'Read the book Road to React',
+    objectID: 1,
+    },
+    {
+    title: 'Watch Youtube videos',
+    objectID: 2,
+    }
+    ];
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {todoList.map(
+          (item)=>{
+            return( 
+              <li key={item.objectID}>
+                {item.title}
+              </li>
+            )
+        })}
+      </ul>
     </div>
   );
 }
